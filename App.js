f10cae91794552b386201c8b94ef66ee8e884cb9
Daniel Ionita <danielionita88@yahoo.com>
2020-04-03 18:48:48 -0500

    split app in components

--- App.js
+++ App.js
@@ -1,33 +1,33 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet,
+  Text, 
+  View, 
+  TextInput, 
+  Button, 
+  ScrollView, 
+  FlatList
+} from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
 
-  const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = enteredText => {
-    setEnteredGoal(enteredText);
-  };
-
-  const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  const addGoalHandler = (goalTitle) => {
+    setCourseGoals(currentGoals => [...currentGoals, {key: Math.random().toString(), value: goalTitle}]);
   };
 
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder='Course Goal'
-          style={styles.input}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
+        <GoalInput 
+          addGoal={addGoalHandler}
         />
-        <Button title='Add' onPress={addGoalHandler}/>
-      </View>
-      <View>
-        {courseGoals.map(goal => <Text>{goal}</Text>)}
-      </View>
+      <FlatList 
+        data={courseGoals} 
+        renderItem={itemData => <GoalItem title={itemData.item.value} />}
+      />
     </View>
   );
 }
@@ -35,16 +35,5 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50
-  },
-  inputContainer: { 
-    flexDirection: 'row',
-    justifyContent: 'space-between', 
-    alignItems: 'center' 
-  },
-  input: { 
-    width: '80%', 
-    borderColor: 'black', 
-    borderWidth: 1, 
-    padding: 10
   }
 });
